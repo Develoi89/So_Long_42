fdd66b6fc87b7076743d44f71010607c3dab8e9c
Eloi Alonso quinonero <ealonso-@car4s1.42barcelona.com>
2022-04-06 18:54:13 +0200

    structsize

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:59:58 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/04/06 17:25:39 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:53:11 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*wndw;
+	t_sizes	sizes;
 
-	mlx_ptr = mlx_init();
-	wndw = mlx_new_window(mlx_ptr, 160, 80, "so_long");
-	mlx_loop(mlx_ptr);
+	sizes.mlx_ptr = mlx_init();
+	sizes.wndw = mlx_new_window(sizes.mlx_ptr, 160, 80, "so_long");
+	mlx_loop(sizes.mlx_ptr);
 	return (0);
 }
