39a916e65bc8abd1112b48161c0afb38764f5c67
Eloi Alonso quinonero <ealonso-@car4s1.42barcelona.com>
2022-04-06 18:52:45 +0200

    t_size added

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 16:16:15 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/04/06 17:20:16 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:43:02 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include "mlx/mlx.h"
 # include "libft/libft.h"
+
+typedef struct s_sizes
+{
+	void	*mlx_ptr;
+	void	*wndw;
+}	t_sizes;
 
 #endif
